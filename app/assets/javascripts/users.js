cab0ab9417ef53eaa5c8a1da4710ffe47244266a
Mitch Joa <mitchjoa@gmail.com>
2017-03-07 14:39:24 -0600

    Initial rails setup and basic select2

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,31 @@
+var Select2Demo = Select2Demo || {};
+
+Select2Demo.Users = function(options) {
+  var getUsers = options.getUsers;
+
+  this.init = function() {
+    bindSelect2();
+  }
+
+  var bindSelect2 = function() {
+    var select2Options = {
+      ajax: {
+        dataType: 'json',
+        url: getUsers,
+        data: function(params) {
+          return {q: params.term}
+        },
+        processResults: function(data, params) {
+          return {
+            results: data.results,
+          };
+        }
+      },
+      allowClear: true,
+      placeholder: $(this).data('placeholder'),
+      width: '100%'
+    };
+
+    $('[data-target=select2]').select2(select2Options);
+  };
+};
